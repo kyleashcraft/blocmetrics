670e125356fc9536b34b834b5763ee6e3df7c587
kyleashcraft <ashcraft.k@husky.neu.edu>
2018-03-21 22:05:00 -0700

    CRUD added for registered applications

--- app/controllers/registered_applications_controller.rb
+++ app/controllers/registered_applications_controller.rb
@@ -1,22 +1,63 @@
 class RegisteredApplicationsController < ApplicationController
   def show
+    @app = RegisteredApplication.find(params[:id])
+    authorize(current_user, @app)
   end
 
   def index
+    authorize_user
+    @apps = RegisteredApplication.where(user: current_user)
   end
 
   def new
+    authorize_user
+    @app = RegisteredApplication.new
   end
 
-  def update
-  end
+  def create
+    authorize_user
+    @app = RegisteredApplication.new(app_params)
+    @app.user = current_user
 
-  def edit
-  end
-
-  def update
+    if @app.save
+      flash[:notice] = "Application was registered"
+      redirect_to @app
+    else
+      flash.now[:alert] = "There was an error registering your application. Please try again."
+      render :new
+    end
   end
 
   def destroy
-  end 
+    @app = RegisteredApplication.find(params[:id])
+
+    authorize(current_user, @app)
+
+    if @app.destroy
+      flash[:notice] = "Registration successfully removed."
+      redirect_to registered_applications_path
+    else
+      flash.now[:alert] = "Error removing application registration, please try again."
+    end
+  end
+
+  private
+
+    def app_params
+      params.require(:registered_application).permit(:name, :url, :user)
+    end
+
+    def authorize_user
+      unless current_user
+        flash[:alert] = "You must be logged in to do that."
+        redirect_to root_path
+      end
+    end
+
+    def authorize(current_user, app)
+      unless current_user.id == app.user_id
+        flash[:alert] = "You are not allowed to do that."
+        redirect_to root_path
+      end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'faker'
 
 5.times do
@@ -20,9 +12,10 @@ users = User.all
 p "#{users.count} users created."
 
 10.times do
+  name = Faker::App.unique.name
   RegisteredApplication.create!(
-    name: Faker::App.unique.name,
-    url: Faker::Internet.unique.url,
+    name: name,
+    url: Faker::Internet.unique.url("#{name.downcase}.com"),
     user: users.sample
   )
 end
