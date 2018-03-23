aad94af0e7ee286b9aa730aaa0b1fe3d2131d332
kyleashcraft <ashcraft.k@husky.neu.edu>
2018-03-23 00:42:47 -0700

    Added event API functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :registered_applications
+
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
+
   root 'welcome#index'
 end
